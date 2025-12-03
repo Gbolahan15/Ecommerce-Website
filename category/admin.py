8c259f68c63bc8e8832382fe19ca4e4c074e2fbf
Gbolahan <adesinagbolahan15@gmail.com>
2025-12-03 16:05:07 +0100

    Prepopulated Category slug

--- category/admin.py
+++ category/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Category
 # Register your models here.
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('category_name',)} # put a comma if a tuple has a single item
+    list_display = ('category_name', 'slug')
+
+admin.site.register(Category, CategoryAdmin)
